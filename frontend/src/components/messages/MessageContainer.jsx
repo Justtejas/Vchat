5d2d5d3fcc58188654e333e7145bd384d34ae591
Justtejas <tejaspantoji@gmail.com>
2024-03-06 22:03:44 +0530

    feat: zustand for global state, conversation

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
+import { useConversation } from "../../state/useConversation";
 
 function MessageContainer() {
-	const notSelected = true;
+	const { selectedConversation, setSelectedConversation } = useConversation();
+	useEffect(() => {
+		return () => {
+			setSelectedConversation(null);
+		};
+	}, [setSelectedConversation]);
 	return (
 		<div className='md:min-w-[450px] flex flex-col'>
-			{notSelected ? (
+			{!selectedConversation ? (
 				<NoChat />
 			) : (
 				<>
 					<div className='bg-slate-700 px-4 py-2 mb-2'>
 						<span className='label-text text-white'>To:</span>{" "}
-						<span className='text-white font-bold'>Lam</span>
+						<span className='text-white font-bold'>
+							{selectedConversation.username}
+						</span>
 					</div>
 					<Messages />
 					<MessageInput />
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import { useConversation } from "../../state/useConversation";
 
-function Conversation() {
+function Conversation({ conversation, emoji, lastIdx }) {
+	const { selectedConversation, setSelectedConversation } = useConversation();
+	const isSelected = selectedConversation?._id === conversation._id;
 	return (
 		<>
-			<div className='flex items-center gap-3 p-2 rounded-md hover:bg-blue-500 cursor-pointer'>
+			<div
+				className={`flex items-center gap-3 p-2 rounded-md hover:bg-blue-500 cursor-pointer ${
+					isSelected ? "bg-sky-400" : ""
+				}`}
+				onClick={() => setSelectedConversation(conversation)}
+			>
 				<div className='avatar online'>
 					<div className='w-12 rounded-full'>
-						<img
-							src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
-							alt='user avatar'
-						/>
+						<img src={conversation.profilePic} alt='user avatar' />
 					</div>
 				</div>
 				<div className='flex flex-col flex-1'>
 					<div className='flex gap justify-between'>
-						<p className='font-bold text-white'>Lam</p>
-						<span className='text-xl'> 🤢</span>
+						<p className='font-bold text-white'>{conversation.username}</p>
+						<span className='text-xl'>{emoji}</span>
 					</div>
 				</div>
 			</div>
-			<div className='divider my-0 py-0 h-1 '></div>
+			{lastIdx ? null : <div className='divider my-0 py-0 h-1 '></div>}
 		</>
 	);
 }
