fd891acbc8c09e2a33ca9a13dc410ea91c0d65af
Justtejas <tejaspantoji@gmail.com>
2024-03-12 20:02:18 +0530

    fix: deployment issues

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth.routes");
 const messageRouter = require("./routes/message.routes");
@@ -7,12 +8,19 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 const { app, server } = require("./socket/socket");
 const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
 
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/user", userRouter);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(_dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
 	connectDB();
 	console.log(`Server is running on port: ${PORT}`);
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketProvider = ({ children }) => {
 	const { authUser } = useAuth();
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000", {
+			const socket = io("https://vchat-7gda.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
