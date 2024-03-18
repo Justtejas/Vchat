9eb9fc1aabcff3606d12b081b09e19c45d5c9a18
Justtejas <tejaspantoji@gmail.com>
2024-03-18 15:14:42 +0530

    refactor url

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth.routes");
@@ -8,18 +9,18 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 const { app, server } = require("./socket/socket");
 const PORT = process.env.PORT || 5000;
-const _dirname = path.resolve();
 
 app.use(express.json());
+app.use(helmet());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/user", userRouter);
 
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.send(path.join(_dirname, "/frontend/dist/index.html"));
+	res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
 });
 server.listen(PORT, () => {
 	connectDB();
